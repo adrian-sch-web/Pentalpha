e444a050f202bca2c9b1cdc126364df4be04701c
Adrian Schaich <SchAd@hbt.local>
2023-12-15 09:06:23 +0100

    implemented Coloring funktionality

--- src/app/boardview/Pentagram.ts
+++ src/app/boardview/Pentagram.ts
@@ -1,3 +1,4 @@
+
 export type Pentagram = Spot[];
 
 export interface Spot {
--- src/app/boardview/boardview.component.ts
+++ src/app/boardview/boardview.component.ts
@@ -9,6 +9,8 @@ import { Pentagram, Color } from './Pentagram';
 export class BoardviewComponent implements OnInit {
   private degreeToRadian = Math.PI / 180;
   private goldenRatio = (1 + Math.sqrt(5)) / 2;
+  private selectedNode?: number;
+
   public pentagram: Pentagram = [];
 
   ngOnInit(): void {
@@ -17,7 +19,6 @@ export class BoardviewComponent implements OnInit {
   }
 
   getPoints(radius: number, startAngle: number) {
-    let points: number[][] = [];
     for (let i = 0; i < 5; i++) {
       this.pentagram.push({
         point: [100 + radius * Math.cos(startAngle * this.degreeToRadian), 100 + radius * Math.sin(startAngle * this.degreeToRadian)],
@@ -37,10 +38,40 @@ export class BoardviewComponent implements OnInit {
   }
 
   getOpacity(index: number): string {
-    return this.pentagram[index].stone ? "1" : "0.1";
+    return this.pentagram[index].stone ? "1" : "0.5";
   }
 
-  test(index: number) {
+  getColorString(color: Color): string {
+    return Color[color];
+  }
+
+  mouseEnter(index: number) {
+    if (!this.pentagram[index].stone) {
+      this.pentagram[index].color = Color.red;
+      for (let point of this.getPossiblePlaces(index)) {
+        this.pentagram[point].color = Color.green;
+      }
+    }
+  }
+
+  getPossiblePlaces(index: number): number[] {
+    let places: number[] = [];
+    if (!this.pentagram[(index + 2) % 5].stone) places.push((index + 2) % 5);
+    if (!this.pentagram[(index + 3) % 5].stone) places.push((index + 3) % 5);
+    if (index <= 4) {
+      places.forEach((a, index, array) => array[index] += 5)
+    }
+    return places;
+  }
+
+  mouseLeave(index: number) {
+    this.pentagram[index].color = Color.black;
+    for (let point of this.getPossiblePlaces(index)) {
+      this.pentagram[point].color = Color.black;
+    }
+  }
+
+  onClick(index: number) {
     
   }
 }
